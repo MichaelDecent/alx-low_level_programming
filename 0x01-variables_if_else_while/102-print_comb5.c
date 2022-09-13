9f4d31c3c67230b6bce776afe9e8d1e2eb059324
MichaelDecent <michaeldecent2@gmail.com>
2022-09-13 00:22:15 +0000

    100-intel

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,24 +14,34 @@ int main(void)
 	{
 		for (b = '0'; b <= '9'; b++)
 		{
-			putchar (a);
-			putchar (b);
-			putchar (' ');
+			for (c = '0'; c <= '9'; c++)
+			{
+				for (d = '0'; c <= '9'; d++)
+				{
+					if (((c + d) > (a + b) && c >= a || a < c)
+					{
+						putchar (a);
+						putchar (b);
+						putchar (' ');
+						putchar (c);
+						putchar (d);
+					
+						if (c + i + k + j == 227 && c == 57)
+							break;
+						else 
+						{
+							putchar (',');
+							putchar (' ');
+						}
+				:wq
+				
 
-		}
-	}
-	for (c = '0'; c <= 9; c++)
-	{
+			
+		
 
-		for (d = c + 1; d <= '8'; d++)
-		{
-			putchar (c);
-			putchar (d);
-			putchar (',');
-			putchar (' ');
-		}
+	
 
-	}
+	
 
 	putchar (10);
 	return (0);
