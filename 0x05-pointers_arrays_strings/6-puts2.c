799059ebaa5585627ebb0798f97b9287c6c62280
MichaelDecent <michaeldecent2@gmail.com>
2022-09-20 01:40:50 +0000

    added a function that prints every other character of a string, starting with the first character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,15 +3,18 @@
  * puts -  a function that prints every other character of a string
  * 
  */
+
 void puts2(char *str)
 {
 	int i;
+
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (i % 2 == 0);
+		if (i % 2 == 0)
 		{
-			_putchar (str[i]);
+			_putchar(str[i]);
 		}
-		
-		_putchar ('\n');
+	}
+	_putchar('\n');
 }
