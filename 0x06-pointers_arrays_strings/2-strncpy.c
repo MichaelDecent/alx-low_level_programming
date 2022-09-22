32a85108be46e89def1006fe44f478b70af365c1
MichaelDecent <michaeldecent2@gmail.com>
2022-09-22 01:09:31 +0000

    added a function that copies a strin

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,10 +8,8 @@
  */
 
 char *_strncpy(char *dest, char *src, int n)
-
 {
 	int i;
-
 
 	i = 0;
 	while (i < n && src[i] != '\0')
@@ -20,7 +18,13 @@ char *_strncpy(char *dest, char *src, int n)
 		i++;
 	}
 
-
 	while (i < n)
+
 	{
-		dest[i] = '\0';
+		dest[i] = '\0';	
+		i++;
+	}
+
+	return (dest);
+
+}
