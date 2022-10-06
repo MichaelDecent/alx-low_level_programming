00bc4a912bc6c66487b85a13e24dd5fd7b770b0c
MichaelDecent <michaeldecent2@gmail.com>
2022-10-06 21:57:38 +0000

    a function that creates an array of integers

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <limits.h>
 
+void *_calloc(unsigned int nmemb, unsigned int size);
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 #endif /*MAIN_H*/
