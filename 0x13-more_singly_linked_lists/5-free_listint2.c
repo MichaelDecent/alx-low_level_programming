de41f0d7732edd7989749ab435f870ffcd87e39e
MichaelDecent <michaeldecent2@gmail.com>
2022-10-26 02:10:37 +0000

    a function that frees a listint_t list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,7 +3,7 @@
  * free_listint2 - A function that frees a structure listint_t
  * @head: A pointer to a node
  */
-void free_listint2(listint_t *head)
+void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
