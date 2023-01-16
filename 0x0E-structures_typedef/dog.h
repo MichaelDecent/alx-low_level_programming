6f17c52a67657bc630a932f107526aecd2d14eac
MichaelDecent <michaeldecent2@gmail.com>
2023-01-16 14:53:46 +0000

    added an header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _dog_h
+#define _dog_h
+/**
+ * struct dog - define a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
