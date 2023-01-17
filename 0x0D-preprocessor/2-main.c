f31e1e6c678b623db6accfab7d98715e4542f405
MichaelDecent <michaeldecent2@gmail.com>
2023-01-17 23:26:32 +0000

    add new a function-like macro SUM(x, y) that computes the sum of the numbers x and ywq

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+
+/**
+ * main - check the code for ALX school students
+ *
+ * Return: returns always zero
+ *
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
