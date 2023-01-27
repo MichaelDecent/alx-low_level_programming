6c383ac3c31ada0c9923487f9519af12ceb0d745
MichaelDecent <michaeldecent2@gmail.com>
2023-01-27 12:29:56 +0000

    a function that prints a text before the main function executed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,8 +9,8 @@ void print_text(void)__attribute__ ((constructor));
 
 void print_text(void)
 {
-	printf("You're beat! and yet, you must allow,
-\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n\
+I bore my house upon my back!\n");
 }
 
 
