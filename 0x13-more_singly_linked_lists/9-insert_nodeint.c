b8ad426a73e99e87c4d36b6a335ca0aab0c1a131
MichaelDecent <michaeldecent2@gmail.com>
2023-01-31 07:58:19 +0000

    a function that returns the number of elements in a linked listint_t list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,7 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	prev = *head;
 	while (i < idx)
 	{
-		if (prev->next == NULL)
+		if (prev == NULL)
 			return (NULL);
 		temp = prev->next;
 		if (i == (idx - 1))
