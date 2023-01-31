93b821465a552576ea4c0fd224a50d28f4576fd4
MichaelDecent <michaeldecent2@gmail.com>
2023-01-31 08:02:52 +0000

    a function that returns the number of elements in a linked listint_t list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,14 +11,15 @@ void free_listint2(listint_t **head)
 
 	if (*head != NULL)
 	{
-		while ((*head)->next != NULL)
+		while (*head != NULL)
 		{
 			temp = (*head)->next;
 			free(*head);
 			*head = temp;
 		}
+		free(*head);
+		*head = NULL;
 	}
-	*head = NULL;
 }
 
 
