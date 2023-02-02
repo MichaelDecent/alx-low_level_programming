6597cd91f7e4912cd9c97ce4845ffa8891972aa9
MichaelDecent <michaeldecent2@gmail.com>
2023-02-02 01:08:58 +0000

    a function that frees a listint_t list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -13,7 +13,7 @@ listint_t *reverse_listint(listint_t **head)
 	if ((*head)->next == NULL)
 		return (*head);
 
-	if (*head == NULL || head == NULL)
+	if (head == NULL || *head == NULL)
 		return (NULL);
 
 	temp = (*head)->next;
