472f94e028b90f01c76ad5e0992a0a04bd5159f2
MichaelDecent <michaeldecent2@gmail.com>
2023-02-06 19:15:52 +0000

    a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,13 +2,14 @@
 /**
  * create_file - a function that creates a file
  * @filename: name of file
- * @text_content
+ * @text_content: a NULL terminated string to write to the file
  *
  * Return:  1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
+
 	if (filename == NULL)
 		return (-1);
 
