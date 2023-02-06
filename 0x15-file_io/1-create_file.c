5dc2e4ef7c2d7c30951e7b1fbee1795aa110dfe2
MichaelDecent <michaeldecent2@gmail.com>
2023-02-06 19:58:26 +0000

    a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,14 +14,21 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = creat(filename, 0600);
-	if (fd != -1)
+	fd = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0600);
+	if (fd < 0)
 	{
-		chk = write(fd, text_content, sizeof(text_content));
-		if (chk == -1)
-			return (-1);
-
-		return (1);
+		if (errno == EEXIST)
+		{
+			open(filename, O_WRONLY | O_TRUNC);
+			return (1);
+		}
+		return (-1);
 	}
-	return (-1);
+
+	chk = write(fd, text_content, sizeof(text_content));
+	if (chk == -1)
+	{
+		return (-1);
+	}
+	return (1);
 }
