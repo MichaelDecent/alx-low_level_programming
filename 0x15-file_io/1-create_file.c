d329c5135a9746af0a1832974ee793eee8507ca7
MichaelDecent <michaeldecent2@gmail.com>
2023-02-06 20:33:37 +0000

    a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 
 	len = 0;
-	while (text_content[len])
+	while (text_content[len] != '\0')
 		len++;
 
 	fd = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0600);
