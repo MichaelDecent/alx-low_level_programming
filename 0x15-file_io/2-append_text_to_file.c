0f0fb3955803f8ba775f4c6fd84e5655545cb9f8
MichaelDecent <michaeldecent2@gmail.com>
2023-02-06 22:34:53 +0000

    a function that creates a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,22 +14,30 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	len = 0;
-	while (text_content[len] != '\0')
-		len++;
+
+	if (text_content == NULL)
+	{
+		text_content = "";
+		len == 1;
+	}
+	else
+	{
+		len = 0;
+		while (text_content[len] != '\0')
+		{
+			len++;
+		}
+	}
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (fd != -1)
 	{
-		if (text_content != NULL)
+		chk = write(fd, text_content, len);
+		if (chk != -1)
 		{
-			chk = write(fd, text_content, len);
-			if (chk != -1)
-			{
-				close(fd);
-				return (1);
-			}
+			close(fd);
+			return (1);
 		}
 	}
 	close(fd);
