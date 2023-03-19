3477198b868ff4114578f71c7333f1693d6237d6
MichaelDecent <michaeldecent2@gmail.com>
2023-03-19 07:37:08 +0000

    function that deletes the node at index index of a dlistint_t linked list

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * delete_dnodeint_at_index - deletes the node at index of a
  * dlistint_t linked list
@@ -10,43 +9,69 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *h1;
-	dlistint_t *h2;
-	unsigned int i;
+	dlistint_t *temp, *hold;
+	unsigned int i = 0, len = 0;
 
-	h1 = *head;
-
-	if (h1 != NULL)
-		while (h1->prev != NULL)
-			h1 = h1->prev;
-
-	i = 0;
-
-	while (h1 != NULL)
+	if (*head == NULL)
+		return (-1);
+	len = list_len(head);
+	temp = *head;
+	if (index > len - 1)
+		return (-1);
+	if (index == 0)
 	{
-		if (i == index)
+		if ((*head)->next == NULL)
 		{
-			if (i == 0)
-			{
-				*head = h1->next;
-				if (*head != NULL)
-					(*head)->prev = NULL;
-			}
-			else
-			{
-				h2->next = h1->next;
-				if (h1->next != NULL)
-					h1->next->prev = h2;
-			}
-
-			free(h1);
+			free(*head);
+			*head = NULL;
 			return (1);
 		}
-		h2 = h1;
-		h1 = h1->next;
+		temp = (*head)->next;
+		free(*head);
+		(*head) = temp;
+		temp->prev = NULL;
+		return (1);
+	}
+	if (index == len - 1)
+	{
+		while (temp->next != NULL)
+		{
+			hold = temp;
+			temp = temp->next;
+		}
+		free(temp);
+		hold->next = NULL;
+		return (1);
+	}
+	while (i < index)
+	{
+		hold = temp;
+		temp = temp->next;
 		i++;
 	}
+	hold->next = temp->next;
+	temp->next->prev = hold;
+	printf("%d\n", temp->n);
+	free(temp);
+	return (1);
+}
+/**
+ * list_len - A function that the length of the list
+ * @head: a pointer to the head node
+ *
+ * Return: length of the list
+ */
+int list_len(dlistint_t **head)
+{
+	int len = 0;
+	dlistint_t *temp;
 
-	return (-1);
+	temp = *head;
+	while (temp != NULL)
+	{
+		temp = (temp)->next;
+		len++;
+	}
+	return (len);
 }
 
