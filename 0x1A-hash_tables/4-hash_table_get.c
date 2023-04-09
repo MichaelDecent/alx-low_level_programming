d57e45469dfb802c6be288893163f7738159aa94
MichaelDecent <michaeldecent2@gmail.com>
2023-04-09 02:48:59 +0000

    a function that retrieves a value associated with a key.

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,9 +12,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *temp;
 
 	if (ht == NULL || key == NULL)
-	{
 		return (NULL);
-	}
+
 	idx = key_index((unsigned char *)key, ht->size);
 	for (i = idx; ht->array[i]; i++)
 	{
