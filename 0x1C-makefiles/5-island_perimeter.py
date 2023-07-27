758fb096d4b10670d26bad47d8934ebc50edcafc
MichaelDecent <michaeldecent2@gmail.com>
2023-07-27 11:17:22 +0000

    update on makefile project

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-"""
-This module defines Island Perimeter
+""" this module defines the perimter of a grid
 """
 
 
 def island_perimeter(grid):
-    """ Calculate perimeter of grid where "1" is found"""
+    """ returns the perimeter of the island described in grid
+            grid(list of integer): describes the highland
+    """
     perimeter = 0
-
     for row in range(len(grid)):
-        for col in range(len(grid[0])):
+        for col in range(len(grid[row])):
             if grid[row][col] == 1:
-                if row == 0 or grid[row - 1][col] == 0:
+                if grid[row - 1][col] == 0 or row == 0:
                     perimeter += 1
-                if row == (len(grid) - 1) or grid[row + 1][col] == 0:
+                if grid[row + 1][col] == 0 or row == (len(grid) - 1):
                     perimeter += 1
-                if col == 0 or grid[row][col - 1] == 0:
+                if grid[row][col - 1] == 0 or col == 0:
                     perimeter += 1
-                if col == (len(grid[0]) - 1) or grid[row][col + 1] == 0:
-                    perimeter += 1
+                if grid[row][col + 1] == 0 or col == (len(grid[row]) - 1):
+                    perimeter += 1 
     return perimeter
