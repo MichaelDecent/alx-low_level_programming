a4406684c452f55663eeb8a6b9f23406995b0f3b
MichaelDecent <michaeldecent2@gmail.com>
2023-09-12 16:29:28 +0000

    added a file for binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,4 +1,4 @@
-#include "search_algos"
+#include "search_algos.h"
 
 /**
 * binary_search - hat searches for a value in an array of
@@ -10,3 +10,32 @@
 * Return: the first index where value is located,
 * -1 if array is NULL of value not found
 */
+
+int binary_search(int *array, size_t size, int value)
+{
+	int left = 0;
+	int right = size - 1;
+
+	if (size == 0)
+		return (-1);
+
+	while (left <= right)
+	{
+		int mid = (left + right) / 2;
+		int i = left;
+		int j = right;
+
+		printf("Searching in array: ");
+		for (; i <= j; i++)
+			printf("%d, ", array[i]);
+		printf("\n");
+
+		if (value == array[mid])
+			return (mid);
+		else if (value < array[mid])
+			right = mid - 1;
+		else
+			left = mid + 1;
+	}
+	return (-1);
+}
