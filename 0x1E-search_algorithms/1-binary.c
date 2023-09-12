4a8ebeb3876141e83cbc76a04094e9a6c2f3fd93
MichaelDecent <michaeldecent2@gmail.com>
2023-09-12 15:52:55 +0000

    added a file that performs linear search

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,12 @@
+#include "search_algos"
+
+/**
+* binary_search - hat searches for a value in an array of
+* integers using the Binary search algorithm
+* @array: a pointer to the first value of the array to search in
+* @size: the number of elements in array
+* @value: the value to search
+*
+* Return: the first index where value is located,
+* -1 if array is NULL of value not found
+*/
